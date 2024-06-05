ac4f8933f244e267525fc809ffa8b1e6fd6cc189
erickmijael17 <erickmijaeldelgafolima@gmail.com>
2024-06-05 09:13:46 -0500

    Update CuentaUnion.java
    MODIFICADO

--- CuentaUnion.java
+++ CuentaUnion.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class CuentaUnion {
-    private String correo, contraseña, correoComp, contraseñaComp;
+    private String correo, contraseña, correo_Comp, contraseña_Comp, nobre, apellido, codigoU;
     public String fdatos() {
         Scanner datos = new Scanner(System.in);
-        String nobre, apellido, codigoU, opcion;
+        String  opcion;
         System.out.println("¿Usted es estudiante de la UPeU?");
         System.out.println("Si/No");
         opcion=datos.nextLine();
@@ -37,11 +37,14 @@ public class CuentaUnion {
         Scanner comprueba= new Scanner(System.in);
         System.out.println("INICIE SESION PARA REALIZAR SU COMPRA");
         System.out.println("Ingrese su correo: ");
-        correoComp=comprueba.nextLine();
+        correo_Comp=comprueba.nextLine();
         System.out.println("Ingrese su cotraseña: ");
-        contraseñaComp=comprueba.nextLine();
+        contraseña_Comp=comprueba.nextLine();
+        if(correo_Comp.equals(correo)&& contraseña_Comp.equals(contraseña)){
 
-        if(correoComp.equals(correo)&&contraseñaComp.equals(contraseña)){
+            System.out.println("Bienvenido a PRODUCTOS UNION");
+            System.out.println("Sr(a): "+nobre+" "+apellido);
+            System.out.println(" "+correo+"@gmail.com");
 
         }
         return null;
